632ae06df9abf27e761a5d7e5d5f5985df579c95
Muhammad Usman Ghias Ali <99526697+UsmanGhias@users.noreply.github.com>
2023-06-09 18:10:37 +0500

    Serialization
    Serialization Done

--- firstproject/snippts/serializers.py
+++ firstproject/snippts/serializers.py
@@ -1,19 +1,5 @@
-from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 from snippts.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
-
-
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ['url', 'username', 'email', 'groups']
-
-
-class GroupSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Group
-        fields = ['url', 'name']
-
 
 #----- Second task---> Snippets
 
